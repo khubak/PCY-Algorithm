ad63c62228c5bc0bcbe628bddbc4d7407d18afa9
Karlo Hubak <103487011+khubak@users.noreply.github.com>
2022-12-22 18:40:24 +0100

    Update PCY.py

--- PCY.py
+++ PCY.py
@@ -1,9 +1,6 @@
 import functions
-#import time
 from collections import defaultdict
 from itertools import combinations
-
-#start_time = time.time()
 
 input_file = functions.parser_sprut()
 
@@ -19,8 +16,6 @@ for kosara in input_file[3:]:
     for predmet in kosara:
         brPredmeta[predmet] += 1
 
-#print('time1 = ', time.time() - start_time)
-
 pretinci = defaultdict(int)
 velicinaBrPredmeta = len(brPredmeta)
 A = int((velicinaBrPredmeta * (velicinaBrPredmeta - 1)) / 2)
@@ -34,38 +29,19 @@ for kosara in input_file[3:]:
             indeksi[j].append(i)
     j += 1
 
-#indeksi = [i for kosara in (input_file[3:]) for i in kosara if brPredmeta[i] >= prag]
-
-#print('time2 = ', time.time() - start_time)
-
 for kosara in indeksi:
 
     for i, j in combinations(kosara, 2):
-            #print(i, j)
-            #if((brPredmeta[i] >= prag) and (brPredmeta[j] >= prag)):
-                pretinci[((i * velicinaBrPredmeta) + j) % brPretinaca] += 1
-
-#print('time3 = ', time.time() - start_time)
+        pretinci[((i * velicinaBrPredmeta) + j) % brPretinaca] += 1
 
 parovi = defaultdict(int)
 
 for kosara in indeksi:
-
-    #duljina_kosare = len(kosara)
-
     for i, j in combinations(kosara, 2):
-            #if(i == j):
-            #    break
-            #if ((brPredmeta[i] >= prag) and (brPredmeta[j] >= prag)):
+        if(pretinci[((i * velicinaBrPredmeta) + j) % brPretinaca] >= prag):
+            parovi[(i, j)] += 1
 
-                if(pretinci[((i * velicinaBrPredmeta) + j) % brPretinaca] >= prag):
-                    parovi[(i, j)] += 1
-
-#print('time4 = ', time.time() - start_time)
-#nadalje isti tajmovi
 P = int(len(parovi))
-
-#print('time5 = ', time.time() - start_time)
 
 print(A)
 print(P)
@@ -73,8 +49,4 @@ print(P)
 pairs = list(parovi.values())
 pairs.sort(reverse=True)
 
-#print('time6 = ', time.time() - start_time)
-
 print(*pairs, sep='\n')
-
-#print('running_time = ', time.time()-start_time)
